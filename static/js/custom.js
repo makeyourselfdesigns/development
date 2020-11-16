238a77159ad959567d08e90fa376af5d3c08f986
Chris LaBarge <chrislabargedev@gmail.com>
2020-11-16 14:28:52 -0500

    style landing page

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,14 +1,16 @@
 window.onload = function() {
-  let form = document.querySelector("form.subscribe")
+  let forms = document.querySelectorAll("form.subscribe")
 
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
+  forms.forEach((form) => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
 
-    fetch(form.action, {
-      method: form.method,
-      body: new FormData(form)
+      fetch(form.action, {
+        method: form.method,
+        body: new FormData(form)
+      })
+
+      form.classList.add("success")
     })
-
-    form.classList.add("success")
   })
 }
